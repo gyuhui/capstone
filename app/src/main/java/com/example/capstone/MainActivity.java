2763c46c16952c1815e74034a1d386d0576bb9c9
문예진 <dpwls991226@naver.com>
2021-08-21 21:39:49 +0900

    connect 클릭시 화면전환

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(),"버튼이 눌렸습니다", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+    public void btn(View v){
+        Toast.makeText(getApplicationContext(),"버튼2가 클릭",Toast.LENGTH_LONG).show();
+    }
+
 }
